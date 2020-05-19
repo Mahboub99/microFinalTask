2ae3345e53f88fffd200dea2d057c2713bff970a
Mahboub99 <ahmed.mahboub99@gmail.com>
2020-05-19 09:04:23 +0200

    add resistor for first line

--- Task3.ino
+++ Task3.ino
@@ -1,36 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
-   for(int i =0 ;i<9 ;i++)pinMode(i ,OUTPUT);
+   for(int i =0 ;i<8 ;i++)pinMode(i ,OUTPUT);
    
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-     digitalWrite(8,1);
+     
+	  digitalWrite(7 ,1 );
+      for(int i = 0 ;i<100 ;i++)
+	  	selectLED(i), delay(100);		
       
-      digitalWrite(0,0);
-      digitalWrite(1,0);
-      digitalWrite(2,0);
-      digitalWrite(3,0);
-
-
-      digitalWrite(4,0);
-      digitalWrite(5,0);
-      digitalWrite(6,0);
-      digitalWrite(7,0);
-      
-     delay(1000);
-      
-      digitalWrite(0,1);
-      digitalWrite(1,0);
-      digitalWrite(2,0);
-      digitalWrite(3,0);
-
-
-      digitalWrite(4,1);
-      digitalWrite(5,0);
-      digitalWrite(6,0);
-      digitalWrite(7,0);
-      delay(1000);
 }
-
+void selectLED(int num){
+ for(int  i =0 ;i<7 ;i++) digitalWrite(i , (1<<i)&num); 
+}
