50afe33d2a0717298fa1d28425c859b068428f7a
Mahboub99 <ahmed.mahboub99@gmail.com>
2020-05-19 06:08:56 +0200

    update Design

--- Task3.ino
+++ Task3.ino
@@ -1,27 +1,36 @@
 void setup() {
   // put your setup code here, to run once:
-   pinMode(1 ,OUTPUT);
-   pinMode(2 ,OUTPUT);
-   pinMode(3 ,OUTPUT);
+   for(int i =0 ;i<9 ;i++)pinMode(i ,OUTPUT);
+   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    
-      digitalWrite(1 , 1);
-      digitalWrite(3 , 0);
-      delay(500);
- 	  digitalWrite(1 , 1);
-      digitalWrite(3 , 1);
-      delay(500);
-	  digitalWrite(1 , 0);
-      digitalWrite(3 , 1);
-      delay(500);
-      digitalWrite(1 , 0);
-      digitalWrite(3 , 0);
-      delay(500);
+     digitalWrite(8,1);
+      
+      digitalWrite(0,0);
+      digitalWrite(1,0);
       digitalWrite(2,0);
+      digitalWrite(3,0);
+
+
+      digitalWrite(4,0);
+      digitalWrite(5,0);
+      digitalWrite(6,0);
+      digitalWrite(7,0);
       
+     delay(1000);
       
+      digitalWrite(0,1);
+      digitalWrite(1,0);
+      digitalWrite(2,0);
+      digitalWrite(3,0);
+
+
+      digitalWrite(4,1);
+      digitalWrite(5,0);
+      digitalWrite(6,0);
+      digitalWrite(7,0);
+      delay(1000);
 }
 
