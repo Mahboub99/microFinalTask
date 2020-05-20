82fb3b034e8e4aaa25636c183327f32c74a199e1
Mahboub99 <ahmed.mahboub99@gmail.com>
2020-05-20 03:22:52 +0200

    update design

--- Task3.ino
+++ Task3.ino
@@ -1,17 +1,19 @@
 void setup() {
   // put your setup code here, to run once:
-   for(int i =0 ;i<8 ;i++)pinMode(i ,OUTPUT);
+   for(int i =0 ;i<9 ;i++)pinMode(i ,OUTPUT);
    
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
      
-	  digitalWrite(7 ,1 );
-      for(int i = 0 ;i<100 ;i++)
-	  	selectLED(i), delay(100);		
+	  digitalWrite(8 ,1 );
+      
+	  selectLED(99);	
       
 }
 void selectLED(int num){
+ digitalWrite(7 , 1);
  for(int  i =0 ;i<7 ;i++) digitalWrite(i , (1<<i)&num); 
+ digitalWrite(7 ,0);
 }
