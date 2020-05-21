ba6020bc40b124c5ca73a764682d619e68698908
Mahboub99 <ahmed.mahboub99@gmail.com>
2020-05-21 10:52:59 +0200

    add switch between modes logic

--- Task3.ino
+++ Task3.ino
@@ -1,16 +1,16 @@
-int key[10] = {911 , 730 , 719 ,708 , 786 , 773 ,761 , 852 ,837 , 822}; 
+int key[10] = {911 , 730 , 719 ,708 , 761 , 773 ,786 , 852 ,837 , 822}; 
 unsigned long int Data[2][11][10] = { //0 numeric , 1 emotions
 								{
-                                     {252, 132 ,132, 132 ,132 ,132 ,132 , 132 , 132 , 252},
-                                     {48 , 48 , 48 , 48 ,48 , 48 , 48 ,48 ,48 ,48},
-								   {0 ,252 , 4 , 4 , 4 ,252 ,128 , 128 ,128 ,252 },
-								   {0 ,252 , 4 , 4 , 4 ,252 ,4 , 4 ,4 ,252 },
-								   {132 , 132 , 132 , 132 ,252 , 4 , 4 ,4 ,4 ,4},
-								   {0 ,252 , 128 , 128 , 128 ,252 ,4 , 4 ,4 ,252 },
-								   {0 ,252 , 128 , 128 , 128 ,252 ,132 , 132 ,132 ,252 },
-								   {252 , 132 , 132 , 4 ,4 , 4 , 4 ,4 ,4 ,4},
-								   {0 ,252 ,132, 132 ,132 ,252 ,132 , 132 , 132 , 252},
-								   {0 ,252 , 132 , 132 , 132 ,252 ,4 , 4 ,4 ,252 },
+                                     {252, 132 ,132, 132 ,132 ,132 ,132 , 132 , 132 , 252},//0
+                                     {48 , 48 , 48 , 48 ,48 , 48 , 48 ,48 ,48 ,48},//1
+								   {0 ,252 , 4 , 4 , 4 ,252 ,128 , 128 ,128 ,252 },//2
+								   {0 ,252 , 4 , 4 , 4 ,252 ,4 , 4 ,4 ,252 },//3
+								   {132 , 132 , 132 , 132 ,252 , 4 , 4 ,4 ,4 ,4},//4
+								   {0 ,252 , 128 , 128 , 128 ,252 ,4 , 4 ,4 ,252 },//5
+								   {0 ,252 , 128 , 128 , 128 ,252 ,132 , 132 ,132 ,252 },//6
+								   {252 , 132 , 132 , 4 ,4 , 4 , 4 ,4 ,4 ,4},//7
+								   {0 ,252 ,132, 132 ,132 ,252 ,132 , 132 , 132 , 252},//8
+								   {0 ,252 , 132 , 132 , 132 ,252 ,4 , 4 ,4 ,252 },//9
 								   {0 , 0 , 0 , 0 ,0 , 0 , 0 ,0 ,0 ,0}
 								  },
 								  {
@@ -18,8 +18,8 @@ unsigned long int Data[2][11][10] = { //0 numeric , 1 emotions
                                      {252, 132 ,258, 330 ,513 ,561 ,330 , 258 , 132 , 252},
 								   {1023 ,513 , 645 , 843 , 513 ,513 ,513 , 633 ,513 ,1023 },
 								   {1023 ,513 , 645 , 843 , 513 ,513 ,513 , 513 ,513 ,1023 },
+								   {1023 ,513 , 645 , 585 , 513 ,513 ,513 , 633 ,513 ,1023 },  
 								   {1023 ,513 , 585 , 585 , 513 ,513 ,513 , 633 ,513 ,1023 },
-								   {1023 ,513 , 645 , 585 , 513 ,513 ,513 , 633 ,513 ,1023 },
 								   {0 ,204 , 306 , 513, 513 ,513 ,258 , 132 ,72 ,48 },
 								   {1023 , 819 , 513 , 513 ,513 , 513 , 771 ,903 ,975 ,1023},
 								   {48 ,120 ,252, 48 ,48 ,48 ,48 , 48 , 48 , 48},
@@ -29,6 +29,13 @@ unsigned long int Data[2][11][10] = { //0 numeric , 1 emotions
 								};
 int currentShap = 10;
 int prevShap = 10;
+int password[4] ={1 ,2 ,3 ,4};
+int lastFour[4]={10 ,10 ,10 ,10};
+int changePassword[4] ={1 ,2 ,1 ,2};
+
+int keyPressed = 10;
+int cnt = 0;
+int displayMode = 0; 
 void setup() {
   // put your setup code here, to run once:
    for(int i =0 ;i<14 ;i++)pinMode(i ,OUTPUT);
@@ -37,18 +44,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:      
+  // put your main code here, to run repeatedly:   
+      
+   
 	  digitalWrite(8 ,1 );
 	  digitalWrite(9 ,1);
-     // if(millis()<2000)welcomeMode();
+    
+      //if(millis()<2000)welcomeMode();
       setCurrentShap();
-	  Serial.println(currentShap);
-	  delay(100);
-	  Mode();
-     /* selectRow(9);	
-      delay(500);
-      resetGrid();
-      delay(500);*/
+	 
+	  
+	
+	 
+	  Mode(displayMode);
+	  
+    
 }
 void selectLED(int num){
  digitalWrite(7 , 1);
@@ -86,8 +96,11 @@ void setCurrentShap(){
  //int b = 10;
  for(int i = 0 ; i<10 ;i++) 
  {
-	if(a == key[i]) 
-	  currentShap = i;
+	if(a == key[i]){
+	 currentShap = i;
+	 updateLastFour(i);
+	}
+	   
  }
  if(prevShap != currentShap){
    resetGrid();
@@ -96,16 +109,52 @@ void setCurrentShap(){
  
 }
 
-void Mode(){
+void Mode(int displayMode){
  for(int i =0 ;i< 10 ;i++){
   for(int j = 0 ;j< 10 ;j++){
-  	if(1<<j&Data[1][currentShap][i]){
+  	if(1<<j&Data[displayMode][currentShap][i]){
 	 selectLED(j+1 + 10*i);
 	 delay(100);
-	 setCurrentShap();
+	 
 	}
+	 setCurrentShap();
+	 handlePassword();
    }
- 
  }
+}
+void handlePassword(){
+
+
+	for(int i = 0 ;i< 4 ;i++) {
+	 Serial.print(lastFour[i]);
+	}
+	 Serial.println();
+	 bool ok = 0;
+	 for(int i = 0 ;i<4 ;i++){
+	  if(password[i]!=lastFour[i]) ok = 1;
+	 }
+	
+	 if(!ok){
+	   displayMode = 1;
+	   for(int i =0 ;i<4 ;i++) lastFour[i] =10;
+	   currentShap = 10;
+	 }
+	 ok = 0;
+	 for(int i = 0 ;i<4 ;i++){
+	  if(password[i]!=lastFour[4-i-1]) ok = 1;
+	 }
+	 if(!ok){
+	   displayMode = 0;
+	   for(int i =0 ;i<4 ;i++) lastFour[i] =10;
+	   currentShap = 10;
+	 }
+
+}
+void updateLastFour(int value){
+	
+	for(int i =0 ;i< 3 ;i++){
+	 lastFour[i] =lastFour[i+1];
+	}
+   lastFour[3] = value ;
 
 }
